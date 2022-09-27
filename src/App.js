7d80acb92438056026f04cf3058d9b1aceef86bb
hasib4 <hasib4noor@gmail.com>
2022-09-27 15:10:45 +0600

    Add desired connectivity with backend created

--- src/App.js
+++ src/App.js
@@ -73,7 +73,22 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
   
@@ -94,7 +109,7 @@ class App extends Component {
         { this.state.route === 'home'
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm 
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -103,7 +118,7 @@ class App extends Component {
           </div>
         : (
           this.state.route === 'signin' 
-          ? <SignIn onRouteChange={this.onRouteChange} />
+          ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           : <Registration loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )
         }
