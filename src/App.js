c6c98d31286daa328888ea74d0f4783de8e34071
hasib4 <hasib4noor@gmail.com>
2022-10-18 15:38:21 +0600

    Some fixes

--- src/App.js
+++ src/App.js
@@ -86,6 +86,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
